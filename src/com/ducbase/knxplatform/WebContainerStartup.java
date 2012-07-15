12162825c7ddd9d69706c2e762516ad57cc7d224
Tom Deckers <tom@ducbase.com>
2012-07-15 20:08:01 +0200

    trying to fix hanging thread in Calimero. Not able to fix right now. Might have to fix in Calimero code.

--- src/com/ducbase/knxplatform/WebContainerStartup.java
+++ src/com/ducbase/knxplatform/WebContainerStartup.java
@@ -7,6 +7,10 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import com.ducbase.knxplatform.adapters.KNXAdapter;
+import tuwien.auto.calimero.log.LogManager;
+import tuwien.auto.calimero.log.LogService;
+
+
 
 public class WebContainerStartup implements ServletContextListener {
 	
@@ -25,9 +29,20 @@ public class WebContainerStartup implements ServletContextListener {
 	public void contextDestroyed(ServletContextEvent event) {
 		logger.info("Context Destroyed");
 		KNXAdapter adapter = (KNXAdapter) event.getServletContext().getAttribute("adapter");
-		adapter.stop();		
+		adapter.stop();
+		
+		// Manually close all LogServices... Tomcat recognizes hung threads when reloading app.
+		// TODO: Bug in calimero code?
+		// TODO: Update - this code doesn't work.  Dispatcher thread in LogService has a method
+		// called quit which is never called.
+		String[] names = LogManager.getManager().getAllLogServices();
+		for(String name: names) {
+			logger.warning("Removing log services (KNX)");
+			LogService service = LogManager.getManager().getLogService(name);
+			service.removeAllWriter(true); // this will close them.
+			LogManager.getManager().removeLogService(name);
+		}
+				
 	}
-
-
 
 }
